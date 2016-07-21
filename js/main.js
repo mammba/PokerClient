cc6f9c7a20b12131649c3dcd886754197d8d9589
bulnabulnab <bul.nab@gmail.com>
2016-07-22 02:48:11 +0300

    Opponent avatars

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ $( document ).ready(function() {
 				<div class="row">\
 					<div class="col-xs-6">\
 						<div class="opponent-avatar">\
-							<img src="http://placehold.it/200x200?text=Opponent">\
+							<img src="http://www.avatarpro.biz/avatar/'+opponent.name+'?s=250">\
 						</div>\
 					</div>\
 					<div class="col-xs-6 opponent-info">\
