0f1b78888b473ffbb50aba22e799ed9e9f74bca1
bulnabulnab <bul.nab@gmail.com>
2016-07-22 11:09:44 +0300

    Balance and all stakes updated

--- js/main.js
+++ js/main.js
@@ -198,9 +198,9 @@ $(document).ready(function() {
 						$("#user-card2").attr("src", "img/cards/"+playerArray[i].cards[1].notation+".png");
 					}
 					playerBalance = playerArray[i].balance;
-					$("#user-balance").text("Balance: "+playerBalance);
+					$("#user-balance").text(playerBalance);
 					$("#user-nickname").text(playerArray[i].name);
-					$("#user-stake").text("Stake: "+playerArray[i].stake);
+					$("#user-stake").text(playerArray[i].stake);
 					$("#user-state").text((playerArray[i].state == "waitForMove"?"I'm making a turn":""));
 				}
 				if (firstPlayerId == null && lastGameState == "waitForStart" && data.data.gameState == "started") {
